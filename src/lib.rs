363678577a60334af1018cb15ce7524ba1a37fc0
Sigurd Sætherø Spangelo <sigurd dot spangelo at gmail dot com>
2025-09-24 05:41:26 +0200

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -10,60 +10,60 @@
 #![feature(maybe_uninit_slice)]
 
 //! Turns a range into a linearly spaced sequence of values.
-//! 
+//!
 //! - [`Linspace::linspace`] returns an iterator.
-//! 
+//!
 //! - [`Linspace::linspace_array`] returns an array.
-//! 
+//!
 //! Only works on bounded ranges like [`Range`](core::ops::Range) and [`RangeInclusive`](core::ops::RangeInclusive).
-//! 
+//!
 //! ## Examples
-//! 
+//!
 //! Both of these will print `[0, 25, 50, 75]`.
-//! 
+//!
 //! ```rust
 //! use linspace::*;
-//! 
+//!
 //! let x: Vec<u32> = (0..100).linspace(4).collect();
 //! assert_eq!(x, [0, 25, 50, 75]);
 //! println!("{:?}", vec);
-//! 
+//!
 //! let y: [u32; 4] = (0..100).linspace_array();
 //! assert_eq!(y, [0, 25, 50, 75]);
 //! println!("{:?}", array);
-//! 
+//!
 //! assert_eq!(x, y);
 //! ```
-//! 
+//!
 //! Both inclusive and exclusive ranges can be used.
 //! And these will print `[0, 25, 50, 75, 100]`.
-//! 
+//!
 //! ```rust
 //! use linspace::*;
-//! 
+//!
 //! let x: Vec<u32> = (0..=100).linspace(5).collect();
 //! assert_eq!(x, [0, 25, 50, 75, 100]);
 //! println!("{:?}", x);
-//! 
+//!
 //! let y: [u32; 5] = (0..=100).linspace_array();
 //! assert_eq!(y, [0, 25, 50, 75, 100]);
 //! println!("{:?}", y);
-//! 
+//!
 //! assert_eq!(x, y);
 //! ```
-//! 
+//!
 //! Want a non-linear range? That's also possible. After all, [`Linspace::linspace`] just returns an [`Iterator`].
-//! 
+//!
 //! ```rust
 //! use linspace::*;
-//! 
+//!
 //! let x: Vec<f32> = (0.0..1.0)
 //!     .linspace(10)
 //!     .map(|z| 10.0f32.powf(z))
 //!     .collect();
 //! println!("{:?}", x);
 //! ```
-//! 
+//!
 //! Very convenient!
 
 moddef::moddef!(
