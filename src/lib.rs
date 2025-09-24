03a79d4b5f2bfa0714a36e5a9ade68a664fde5fd
Sigurd Sætherø Spangelo <sigurd dot spangelo at gmail dot com>
2025-09-24 05:16:32 +0200

    Docs

--- src/lib.rs
+++ src/lib.rs
@@ -9,25 +9,66 @@
 #![feature(maybe_uninit_array_assume_init)]
 #![feature(maybe_uninit_slice)]
 
+//! Turns a range into a linearly spaced sequence of values.
+//! 
+//! - [`Linspace::linspace`] returns an iterator.
+//! 
+//! - [`Linspace::linspace_array`] returns an array.
+//! 
+//! Only works on bounded ranges like [`Range`](core::ops::Range) and [`RangeInclusive`](core::ops::RangeInclusive).
+//! 
+//! ## Examples
+//! 
+//! Both of these will print `[0, 25, 50, 75]`
+//! 
+//! ```rust
+//! use linspace::*;
+//! 
+//! let vec: Vec<u32> = (0..100).linspace(4).collect();
+//! println!("{:?}", vec);
+//! 
+//! let array: [u32; 4] = (0..100).linspace_array();
+//! println!("{:?}", array);
+//! 
+//! assert_eq!(vec, array);
+//! assert_eq!(vec, [0, 25, 50, 75]);
+//! assert_eq!(array, [0, 25, 50, 75]);
+//! ```
+//! 
+//! Both inclusive and exclusive ranges can be used.
+//! And these will print `[0, 25, 50, 75, 100]`
+//! 
+//! ```rust
+//! use linspace::*;
+//! 
+//! let vec_inclusive: Vec<u32> = (0..=100).linspace(5).collect();
+//! println!("{:?}", vec_inclusive);
+//! 
+//! let array_inclusive: [u32; 5] = (0..=100).linspace_array();
+//! println!("{:?}", array_inclusive);
+//! 
+//! assert_eq!(vec_inclusive, array_inclusive);
+//! assert_eq!(vec_inclusive, [0, 25, 50, 75, 100]);
+//! assert_eq!(array_inclusive, [0, 25, 50, 75, 100]);
+//! ```
+//! 
+//! Want a non-linear range? That's also possible. After all, [`Linspace::linspace`] just returns an [`Iterator`].
+//! 
+//! ```rust
+//! use linspace::*;
+//! 
+//! let vec: Vec<f32> = (0.0..1.0)
+//!     .linspace(10)
+//!     .map(|x| 10.0f32.powf(x))
+//!     .collect();
+//! println!("{:?}", vec);
+//! ```
+//! 
+//! Very convenient!
+
 moddef::moddef!(
     flat(pub) mod {
         linspace,
         linspaced
     }
 );
-
-#[cfg(test)]
-mod test
-{
-    use crate::{Linspace};
-
-    #[test]
-    fn test()
-    {
-        let t: [f32; 4] = (0.0..1.0).linspace_array();
-        println!("{:?}", t);
-        
-        let t: Vec<f32> = (0.0..1.0).linspace(4).collect();
-        println!("{:?}", t);
-    }
-}
--- src/linspace.rs
+++ src/linspace.rs
@@ -1,25 +1,82 @@
-use core::{mem::MaybeUninit, ops::{Add, Range, RangeBounds, RangeInclusive, Sub}};
+use core::{mem::MaybeUninit, ops::{Add, Range, RangeInclusive, Sub}};
 
 use numscale::NumScale;
 
 use crate::Linspaced;
 
 #[const_trait]
-pub trait Linspace<T>: RangeBounds<T> + Sized
+pub trait Linspace<T>: Sized
 {
     type Output: ExactSizeIterator<Item = T>;
 
+    /// Returns an iterator of evenly spaced values. `count` must be specified.
+    /// 
+    /// # Example
+    /// 
+    /// ```rust
+    /// use linspace::*;
+    /// 
+    /// let x: Vec<u32> = (0..100).linspace(4).collect();
+    /// assert_eq!(x, [0, 25, 50, 75]);
+    /// 
+    /// let x: Vec<u32> = (0..=100).linspace(5).collect();
+    /// assert_eq!(x, [0, 25, 50, 75, 100]);
+    /// ```
+    fn linspace(&self, count: usize) -> Self::Output;
+
+    /// Returns an array of evenly spaced values.
+    /// 
+    /// # Example
+    /// 
+    /// ```rust
+    /// use linspace::*;
+    /// 
+    /// let x: [u32; 4] = (0..100).linspace_array();
+    /// assert_eq!(x, [0, 25, 50, 75]);
+    /// 
+    /// let x: [u32; 5] = (0..=100).linspace_array();
+    /// assert_eq!(x, [0, 25, 50, 75, 100]);
+    /// ```
     fn linspace_array<const N: usize>(&self) -> [T; N]
     {
         let mut array = [const {MaybeUninit::uninit()}; N];
         unsafe {
-            self.linspace_slice_uninit(&mut array);
+            self.linspace_uninit_slice(&mut array);
             MaybeUninit::array_assume_init(array)
         }
     }
+
+    /// Fills a slice with evenly spaced values.
+    /// 
+    /// # Example
+    /// 
+    /// ```rust
+    /// use linspace::*;
+    /// 
+    /// let mut x = [0, 0, 0, 0, 0];
+    /// 
+    /// (0..=100).linspace_slice(&mut x);
+    /// assert_eq!(x, [0, 25, 50, 75, 100]);
+    /// ```
     fn linspace_slice(&self, slice: &mut [T]);
-    unsafe fn linspace_slice_uninit<'a>(&self, slice: &'a mut [MaybeUninit<T>]) -> &'a mut [T];
-    fn linspace(&self, count: usize) -> Self::Output;
+
+    /// Fills a slice with evenly spaced values.
+    /// 
+    /// # Example
+    /// 
+    /// ```rust
+    /// use core::mem::MaybeUninit;
+    /// 
+    /// use linspace::*;
+    /// 
+    /// let mut x = [const {MaybeUninit::uninit()}; 5];
+    /// 
+    /// let x = unsafe {
+    ///     (0..=100).linspace_uninit_slice(&mut x)
+    /// };
+    /// assert_eq!(x, [0, 25, 50, 75, 100]);
+    /// ```
+    unsafe fn linspace_uninit_slice<'a>(&self, slice: &'a mut [MaybeUninit<T>]) -> &'a mut [T];
 }
 
 macro_rules! impl_linspace {
@@ -32,7 +89,13 @@ macro_rules! impl_linspace {
             {
                 type Output = Linspaced<T, $incl>;
 
-                unsafe fn linspace_slice_uninit<'a>(&self, slice: &'a mut [MaybeUninit<T>]) -> &'a mut [T]
+                fn linspace(&self, count: usize) -> Self::Output
+                {
+                    let $this = self;
+                    let (start, end): (T, T) = $bounds;
+                    Linspaced::new(start, end, count)
+                }
+                unsafe fn linspace_uninit_slice<'a>(&self, slice: &'a mut [MaybeUninit<T>]) -> &'a mut [T]
                 {
                     let mut iter = self.linspace(slice.len());
                     loop
@@ -59,12 +122,6 @@ macro_rules! impl_linspace {
                         slice[i] = iter.next_unchecked()
                     }
                 }
-                fn linspace(&self, count: usize) -> Self::Output
-                {
-                    let $this = self;
-                    let (start, end): (T, T) = $bounds;
-                    Linspaced::new(start, end, count)
-                }
             }
         )*
     };
@@ -74,3 +131,47 @@ impl_linspace!(
     Range => false => |range| (range.start, range.end);
     RangeInclusive => true => |range| (*range.start(), *range.end());
 );
+
+#[cfg(test)]
+mod test
+{
+    use crate::{Linspace};
+
+    #[test]
+    fn linspace()
+    {
+        let vec: Vec<u32> = (0..100).linspace(4).collect();
+        println!("{:?}", vec);
+
+        let array: [u32; 4] = (0..100).linspace_array();
+        println!("{:?}", array);
+
+        assert_eq!(vec, array);
+        assert_eq!(vec, [0, 25, 50, 75]);
+        assert_eq!(array, [0, 25, 50, 75]);
+    }
+
+    #[test]
+    fn linspace_inclusive()
+    {
+        let vec_inclusive: Vec<u32> = (0..=100).linspace(5).collect();
+        println!("{:?}", vec_inclusive);
+
+        let array_inclusive: [u32; 5] = (0..=100).linspace_array();
+        println!("{:?}", array_inclusive);
+
+        assert_eq!(vec_inclusive, array_inclusive);
+        assert_eq!(vec_inclusive, [0, 25, 50, 75, 100]);
+        assert_eq!(array_inclusive, [0, 25, 50, 75, 100]);
+    }
+
+    #[test]
+    fn logspace()
+    {
+        let vec: Vec<f32> = (0.0..1.0)
+            .linspace(10)
+            .map(|x| 10.0f32.powf(x))
+            .collect();
+        println!("{:?}", vec);
+    }
+}
